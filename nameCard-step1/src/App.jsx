37230f046f401a741fa56215b3c09ef4973a6c41
junhwan1310 <junhwan1310@gmail.com>
2025-12-31 10:50:52 +0900

    네임카드-실습 초기화

---
+++ nameCard-step1/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+//리액트에서는 태그를 만들어 쓴다.
+//주의: 컴포넌트의 이름을 반드시 대문자로 쓴다.
+//화살표 함수로 변경한 부분
+//() => ()
+//() => {}
+const App = () => {
+  //선언부
+  return (
+    <div className="App">
+      hello react!!
+    </div>
+  );
+}
+
+export default App;
